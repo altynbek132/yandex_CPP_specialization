aac29d36c2ec79f41b61bf6a8b3d2baa6781d547
altynbek132 <altynbek.ktl@gmail.com>
2021-07-24 17:20:11 +0600

    formatting

--- 4__brown_belt/final/transportnyi-spravochnik-chast-a/Request.cpp
+++ 4__brown_belt/final/transportnyi-spravochnik-chast-a/Request.cpp
@@ -19,23 +19,29 @@ RequestHolder RequestBase::Create(RequestBase::Type type) {
             return nullptr;
     }
 }
+
+//
 void AddBusStop::ParseFrom(string_view input) {}
 void AddBusStop::Process(BusManager& manager) const {
     manager.AddBusStop(stop);
 }
 AddBusStop::AddBusStop() : ModifyRequest(RequestBase::Type::ADD_BUS_STOP) {}
+
+//
 void AddBusRoute::ParseFrom(string_view input) {}
 void AddBusRoute::Process(BusManager& manager) const {
     manager.AddBusRoute(bus_route);
 }
 AddBusRoute::AddBusRoute() : ModifyRequest(RequestBase::Type::ADD_BUS_ROUTE) {}
 
+//
 void ReadBusRouteInfo::ParseFrom(string_view input) {}
 ReadBusRouteInfoResultType ReadBusRouteInfo::Process(const BusManager& manager) const {
     return manager.ReadBusRouteInfo(bus_name);
 }
 ReadBusRouteInfo::ReadBusRouteInfo() : ReadRequest(RequestBase::Type::READ_BUS_ROUTE) {}
 
+//
 optional<RequestBase::Type> ConvertRequestTypeFromString(string_view type_str, OperationType operation_type) {
     const auto map_it = OPERATION_TYPE_TO_MAP.find(operation_type);
     if (map_it == OPERATION_TYPE_TO_MAP.end()) {

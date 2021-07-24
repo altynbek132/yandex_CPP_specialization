bc63285065dd8353292dcfbac611762d3fc9ea5a
altynbek132 <altynbek.ktl@gmail.com>
2021-07-25 00:38:43 +0600

    read all requests

--- 4__brown_belt/final/transportnyi-spravochnik-chast-a/main.cpp
+++ 4__brown_belt/final/transportnyi-spravochnik-chast-a/main.cpp
@@ -80,8 +80,23 @@ int main(int argc, const char** argv) {
     cout.precision(6);
 #endif  // MASLO
 
-    const auto requests = ReadRequests();
-    const auto responses = ProcessRequests(requests);
+    auto modify_requests = ReadRequests();
+    auto read_requests = ReadRequests();
+    vector<unique_ptr<Request::Base>> all_requests;
+    all_requests.reserve(modify_requests.size() + read_requests.size());
+    all_requests.insert(all_requests.end(), make_move_iterator(modify_requests.begin()),
+                        make_move_iterator(modify_requests.end()));
+    all_requests.insert(all_requests.end(), make_move_iterator(read_requests.begin()),
+                        make_move_iterator(read_requests.end()));
+
+#ifdef MASLO
+    for (auto& request : all_requests) {
+        std::cout << *request << std::endl;
+    }
+    std::cout << "====\n\n\n" << std::endl;
+#endif  // MASLO
+
+    const auto responses = ProcessRequests(all_requests);
     PrintResponses(responses);
 
     return 0;

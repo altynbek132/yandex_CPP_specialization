e1a7799ea25dc7e2433208b038d75b108c88b1d0
altynbek132 <altynbek.ktl@gmail.com>
2021-07-27 23:28:50 +0600

    add json format for responses

--- 4__brown_belt/final/transportnyi-spravochnik-chast-d/Response.cpp
+++ 4__brown_belt/final/transportnyi-spravochnik-chast-d/Response.cpp
@@ -1,5 +1,6 @@
 #include "Response.h"
 
+using namespace std;
 
 namespace Response {
 
@@ -17,12 +18,45 @@ void BusRouteFound::print(std::ostream& os) const {
        << route_length << " route length, "           //
        << curvature << " curvature";
 }
+Json::Node BusRouteFound::ToJson() const {
+    /*
+  {
+    "route_length": 5950,
+    "request_id": 1965312327,
+    "curvature": 1.36124,
+    "stop_count": 6,
+    "unique_stop_count": 5
+  },
+     * */
+    using namespace Json;
+    map<string, Node> result;
+    result.emplace("route_length", route_length);
+    result.emplace("request_id", request_id);
+    result.emplace("curvature", curvature);
+    result.emplace("stop_count", static_cast<int>(stops_count));
+    result.emplace("unique_stop_count", static_cast<int>(unique_stops_count));
+    return Node(move(result));
+}
 
 void BusRouteNotFound::print(std::ostream& os) const {
     // example:
     // Bus 751: not found
     os << "Bus " << bus_name << ": not found";
 }
+Json::Node BusRouteNotFound::ToJson() const {
+    /*
+  {
+    "request_id": 194217464,
+    "error_message": "not found"
+  },
+     * */
+    using namespace Json;
+    map<string, Node> result;
+    result.emplace("request_id", request_id);
+    result.emplace("error_message", "not found");
+    return Node(move(result));
+}
+
 void BusStopsFound::print(std::ostream& os) const {
     // example:
     // Stop X: buses bus1 bus2 ... busN
@@ -31,14 +65,66 @@ void BusStopsFound::print(std::ostream& os) const {
         os << " " << bus_name;
     }
 }
+Json::Node BusStopsFound::ToJson() const {
+    /*
+  {
+    "buses": [
+      "256",
+      "828"
+    ],
+    "request_id": 1042838872
+  }
+     * */
+    using namespace Json;
+    map<string, Node> result;
+    result.emplace("request_id", request_id);
+
+    vector<Node> buses;
+    buses.reserve(bus_names->size());
+    for (auto& bus_name : *bus_names) {
+        buses.emplace_back(bus_name);
+    }
+    result.emplace("buses", buses);
+    return Node(move(result));
+}
+
 void BusStopsNotFound::print(std::ostream& os) const {
     // example:
     // Stop X: not found
     os << "Stop " << stop_name << ": not found";
 }
+Json::Node BusStopsNotFound::ToJson() const {
+    /*
+  {
+    "request_id": 746888088,
+    "error_message": "not found"
+  },
+     * */
+    using namespace Json;
+    map<string, Node> result;
+    result.emplace("request_id", request_id);
+    result.emplace("error_message", "not found");
+    return Node(move(result));
+}
+
 void BusStopsEmpty::print(std::ostream& os) const {
     // example:
     // Stop X: no buses
     os << "Stop " << stop_name << ": no buses";
 }
+Json::Node BusStopsEmpty::ToJson() const {
+    /*
+  {
+    "buses": [],
+    "request_id": 65100610
+  },
+     * */
+
+    using namespace Json;
+    map<string, Node> result;
+    result.emplace("request_id", request_id);
+    result.emplace("buses", vector<Node>{});
+    return Node(move(result));
+}
+
 }  // namespace Response
--- 4__brown_belt/final/transportnyi-spravochnik-chast-d/Response.h
+++ 4__brown_belt/final/transportnyi-spravochnik-chast-d/Response.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <bits/stdc++.h>
+#include "json.h"
 
 namespace Response {
 
@@ -8,7 +9,9 @@ struct Base;
 using Holder = std::shared_ptr<Base>;
 
 struct Base {
+    int request_id = -1;
     virtual void print(std::ostream& os) const = 0;
+    virtual Json::Node ToJson() const = 0;
 };
 std::ostream& operator<<(std::ostream& out, const Base& response);
 
@@ -21,10 +24,12 @@ struct BusRouteFound : Base {
     double route_length;
     double curvature;
     void print(std::ostream& os) const override;
+    Json::Node ToJson() const override;
 };
 struct BusRouteNotFound : Base {
     std::string_view bus_name;
     void print(std::ostream& os) const override;
+    Json::Node ToJson() const override;
 };
 
 //
@@ -32,14 +37,17 @@ struct BusStopsFound : Base {
     std::string stop_name;
     const std::set<std::string>* bus_names;
     void print(std::ostream& os) const override;
+    Json::Node ToJson() const override;
 };
 struct BusStopsNotFound : Base {
     std::string stop_name;
     void print(std::ostream& os) const override;
+    Json::Node ToJson() const override;
 };
 struct BusStopsEmpty : Base {
     std::string stop_name;
     void print(std::ostream& os) const override;
+    Json::Node ToJson() const override;
 };
 
 }  // namespace Response

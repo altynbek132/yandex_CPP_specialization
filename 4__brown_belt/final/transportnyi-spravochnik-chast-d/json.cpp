e2dd5863b17c5aa29cddffee9547ffe8e0c533ea
altynbek132 <altynbek.ktl@gmail.com>
2021-07-27 21:19:46 +0600

    json input format change: istream to string_view

--- 4__brown_belt/final/transportnyi-spravochnik-chast-d/json.cpp
+++ 4__brown_belt/final/transportnyi-spravochnik-chast-d/json.cpp
@@ -1,4 +1,5 @@
 #include "json.h"
+#include "utils.h"
 
 using namespace std;
 
@@ -10,14 +11,14 @@ const Node& Document::GetRoot() const {
     return root;
 }
 
-Node LoadNode(istream& input);
+Node LoadNode(string_view& input);
 
-Node LoadArray(istream& input) {
+Node LoadArray(string_view& input) {
     vector<Node> result;
 
-    for (char c; input >> c && c != ']';) {
-        if (c != ',') {
-            input.putback(c);
+    for (char c; c = input[0], !input.empty() && c != ']';) {
+        if (c == ',') {
+            input.remove_prefix(1);
         }
         result.push_back(LoadNode(input));
     }
@@ -25,55 +26,76 @@ Node LoadArray(istream& input) {
     return Node(move(result));
 }
 
-Node LoadInt(istream& input) {
+Node LoadInt(string_view& input) {
     int result = 0;
-    while (isdigit(input.peek())) {
+    while (isdigit(input[0])) {
         result *= 10;
-        result += input.get() - '0';
+        result += ReadChar(input) - '0';
     }
     return Node(result);
 }
 
-Node LoadString(istream& input) {
-    string line;
-    getline(input, line, '"');
+string_view ReadDouble(string_view input) {
+    uint8_t dot_count = 0;
+    return ReadTokenIf(input, [&dot_count](const auto ch) {
+        if (isdigit(ch)) {
+            return true;
+        }
+        if (ch == '.') {
+            return ++dot_count <= 1;
+        }
+        return false;
+    });
+}
+
+Node LoadDouble(string_view& input) {
+    return Node(ConvertToDouble(ReadDouble(input)));
+}
+
+Node LoadString(string_view& input) {
+    string line = string(ReadToken(input, "\""));
     return Node(move(line));
 }
 
-Node LoadDict(istream& input) {
+Node LoadDict(string_view& input) {
     map<string, Node> result;
 
-    for (char c; input >> c && c != '}';) {
+    for (char c; c = ReadChar(input), !input.empty() && c != '}';) {
         if (c == ',') {
-            input >> c;
+            c = ReadChar(input);
         }
 
         string key = LoadString(input).AsString();
-        input >> c;
+        c = ReadChar(input);
         result.emplace(move(key), LoadNode(input));
     }
 
     return Node(move(result));
 }
 
-Node LoadNode(istream& input) {
-    char c;
-    input >> c;
+Node LoadNode(string_view& input) {
+    const char c = input[0];
+    auto first_char_removed_input = input.substr(1);
 
     if (c == '[') {
-        return LoadArray(input);
+        return LoadArray(input = first_char_removed_input);
     } else if (c == '{') {
-        return LoadDict(input);
+        return LoadDict(input = first_char_removed_input);
     } else if (c == '"') {
-        return LoadString(input);
+        return LoadString(input = first_char_removed_input);
     } else {
-        input.putback(c);
+        const auto [lhs, rhs] = SplitTwo(input, [](const auto ch) { return isdigit(ch); });
+        if (!rhs.empty() && rhs[0] == '.') {
+            return LoadDouble(input);
+        }
         return LoadInt(input);
     }
 }
 
 Document Load(istream& input) {
-    return Document{LoadNode(input)};
+    string input_str(std::istreambuf_iterator<char>(input), {});
+    string_view input_sv = input_str;
+    return Document{LoadNode(input_sv)};
 }
 
 }  // namespace Json
--- 4__brown_belt/final/transportnyi-spravochnik-chast-d/json.h
+++ 4__brown_belt/final/transportnyi-spravochnik-chast-d/json.h
@@ -8,13 +8,14 @@
 
 namespace Json {
 
-class Node : std::variant<std::vector<Node>, std::map<std::string, Node>, int, std::string> {
+class Node : std::variant<std::vector<Node>, std::map<std::string, Node>, int, double, std::string> {
    public:
     using variant::variant;
 
     const auto& AsArray() const { return std::get<std::vector<Node>>(*this); }
     const auto& AsMap() const { return std::get<std::map<std::string, Node>>(*this); }
     int AsInt() const { return std::get<int>(*this); }
+    double AsDouble() const { return std::get<double>(*this); }
     const auto& AsString() const { return std::get<std::string>(*this); }
 };
 
